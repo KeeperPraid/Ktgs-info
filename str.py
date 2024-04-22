f7fd5c7b426b7cab1a4ccebe52ef6b460c7f85a2
KeeperPraid <167519413+KeeperPraid@users.noreply.github.com>
2024-04-22 12:22:46 +0300

    Add files via upload
    Задание по строкам 22.04.24

---
+++ str.py
@@ -0,0 +1,51 @@
+# ## Рассчет количества книг на дискете
+# disk = 1.44 * 1024 * 1024
+# lists = 100
+# stroks = 50
+# simb = 25 * 4
+#
+# same:int = disk/ (lists*stroks*simb)
+# same = round (same,o)
+# print ("Количество книг", same)
+#
+#  ## Расчет периметра и площади геометрических фигур
+# r = 5
+# a = 5
+# p = 3.1415
+# plk = p*(r**2)
+# dl = 2*p
+# perk = 4*a
+# plkv = a**2
+
+# rplk = round (plk,2)
+# rdl = round (dl,2)
+
+# print ("Площадь круга", rplk)
+# print ("Длина окружности", rdl)
+# print ("Периметр квадрата", perk)
+# print ("Площадь квадрата", plkv)
+
+ ## Строки повторяющихся чисел
+# str_numbers = "0000000000000000000011111111111111111111111111111111111111111111111111222222222222222222222222222222"
+# print (str_numbers)
+# a = str_numbers.count("0")
+# b = str_numbers.count("1")
+# c = str_numbers.count("2")
+# print (a,b,c)
+
+# Футбол
+# lenght_footbal = 90
+# width_footbal = 50
+# perimeter = lenght_footbal * 2 + width_footbal * 2
+# print ("Периметр футбольного поля равен", perimeter)
+
+##Разворот слова
+# print ("Сервис по реверсу слова")
+# word = input()
+# reversed_word = word [::-1]
+# print ("Ваше слово", word,"перевёрнутое слово", reversed_word)
+
+##Уникальные символы в строке
+#str_ = input("Введите строку")
+#str_1 = set(str_)
+#print("Количество уникальных символов в строке - ",len(str_1))
