45e778c557a9c4c1a9f72a804dc41a3251b4926e
KeeperPraid <167519413+KeeperPraid@users.noreply.github.com>
2024-05-29 11:03:43 +0300

    Add files via upload

---
+++ Task_29_05.py
@@ -0,0 +1,27 @@
+# list_mountains = [2650,8140, 7120,6950,8870]
+# print (max(list_mountains))
+# list_mountains = [2650,8140, 7120,6950,8870]
+# m = 0
+# for i in list_mountains:
+#     if i > m:
+#         m = i
+# print (i)
+
+# print ("*")
+# print ("**")
+# print ("***")
+# print ("****")
+# print ("*****")
+# print ("******")
+# print ("*******")
+
+# a, b, c= 0, "", "*"
+# while a < 8:
+#     a+=1
+#     b+=c
+#     print (b)
+
+# list_mountains = [2650,8140, 7120,6950,8870]
+# name_mountains = {2650:'Эльбрус',8140:'Нундзе',7120:'Аннапурна',6950:'Аконкагуа',8870:'Эверест'}
+# b = max(list_mountains)
+# print (name_mountains.get(b))
